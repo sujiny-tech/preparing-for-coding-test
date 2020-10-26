feed1d903edcd024c6914dc7358b7c8422ffbe17
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-10-26 15:09:56 +0900

    Update Gas_Station.py

--- leetcode/Gas_Station.py
+++ leetcode/Gas_Station.py
@@ -12,10 +12,8 @@ else:
     start, fuel=0,0
     for i in range(len(gas)):
         if gas[i]+fuel<cost[i]:
-            print("연료없다, 다시 리셋", fuel)
             start=i+1
             fuel=0
         else:
             fuel+=gas[i]-cost[i]
-            print("연로 있다, 다음번으로", fuel)
 print(start)
