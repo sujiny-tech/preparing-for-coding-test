d39f18de570d038f975f7928eed2d92067ca248a
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-10-26 15:12:40 +0900

    Update Two_Sum.py

--- leetcode/Two_Sum.py
+++ leetcode/Two_Sum.py
@@ -2,13 +2,7 @@
 #target = 9
 nums=[3,3]
 target=6
-'''
-for i in range(len(nums)):
-    for j in range(i+1, len(nums)):
-        if nums[i]+nums[j]==target:
-            print(i, j)
-            break
-'''
+
 for i in range(len(nums)):
     if target-nums[i] in nums[i+1:]:
         print(i, nums[i+1:].index(target-nums[i])+(i+1))
