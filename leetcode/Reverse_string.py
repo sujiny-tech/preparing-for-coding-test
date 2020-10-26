c1460ee203e35678a7325dcc88980fc1d2a18ddd
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-10-26 15:12:21 +0900

    Update Reverse_string.py

--- leetcode/Reverse_string.py
+++ leetcode/Reverse_string.py
@@ -1,13 +1,6 @@
 s=["h","e","l","l","o"]
 result=""
-'''
-for c in s:
-    result+=c
-result=result[::-1]
 
-for i in range(len(s)):
-    s[i]=result[i]
-'''
 mid=len(s)//2
 for i in range(mid):
     tmp=s[i]
