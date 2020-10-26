d196e7fbb5022dfc8a7bbfe7971b70666af10401
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-10-26 15:07:32 +0900

    Update Best_Time_to_Buy_and_Sell_stock.py

--- leetcode/Best_Time_to_Buy_and_Sell_stock.py
+++ leetcode/Best_Time_to_Buy_and_Sell_stock.py
@@ -6,7 +6,6 @@ prices=[7,1,5,3,6,4]
 #Say you have an array for which the i-th element is the price of a given stock on day i.
 #if you were only permiitted to complete at most one transaction,
 #design an algorithm to find th maximunm profit.
-#한번 주식을 사고 팔수있을때 얻을수있는 최대 이윤을 구해라.
 ans=0
 min_val=sys.maxsize
 for price in prices:
