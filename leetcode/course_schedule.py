771746df28be97c7aacb9fef11771446dc29decb
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-11-07 19:10:04 +0900

    Add files via upload

--- leetcode/course_schedule.py
+++ leetcode/course_schedule.py
@@ -1,37 +1,40 @@
 import collections
-num_courses=3
-prerequisites=[[1,0], [0,2], [2,1]]
 
-def canFinish(numCourses, prerequisites):
-    results=[]
-    graph=collections.defaultdict(list)
-    for a, b in prerequisites:
-        graph[a].append(b)
-    print(graph)
+numCourese=3
+#prerequisites=[[1,0]]
+prerequisites=[[0,1], [0,2], [1,2]]#[[1,0], [0,2], [1,2]]
 
-    traced=set()
-    visited=set()
+q=collections.defaultdict(list)
+for a,b in prerequisites:
+    q[a].append(b)
+    #q[a].append(a)
+print(q)
 
-    def dfs(i):
-        if i in traced:
-            return False
-        if i in visited:
-            return True
+def canFinish(numCourese, q):
+    s=[]
+    check=True
+    def dfs(num):
+        c=True
+        print(num, s)
+        if num in s:
+            c=False
+            print("순환!!", c)
+            return c
+        else:
+            s.append(num)
+            for i in q[num]:
+                if dfs(i)==False:
+                    c=False
+                    break
+            return c
 
-        traced.add(i)
+    for i in range(numCourese):
+        s=[]
+        if check:
+            check=dfs(i)
+        else:
+            break
+    print(check)
 
-        for y in graph[i]:
-            if not dfs(y):
-                return False
+print(canFinish(numCourese, q))
 
-        traced.remove(i)
-        visited.add(i)
-
-        return True
-
-    for x in list(graph):
-        if not dfs(x):
-            return False
-    return True
-
-print(canFinish(num_courses, prerequisites))
