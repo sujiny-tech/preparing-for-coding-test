3754cfe4eaf486efac8eec1d20585c96d9e71852
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-11-17 11:08:15 +0900

    Add files via upload

---
+++ leetcode/Convert_Binary_Number_in_a_Linked_List_to_Integer.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        #root = collections.deque([head])
+        bin_val = "0b"
+
+        while head:
+            bin_val += str(head.val)
+            head = head.next
+
+        return int(bin_val, 2)
+
+param_3=ListNode(1)
+param_2=ListNode(0, param_3)
+param_1=ListNode(1, param_2)
+
+res=Solution().getDecimalValue(param_1)
