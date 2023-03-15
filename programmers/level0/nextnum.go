339ba1fda9e7743da663cc2f8c861eb1ae8b4c69
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2023-03-15 15:07:24 +0900

    Add files via upload

---
+++ programmers/level0/nextnum.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solution(common []int) int {
+	total := len(common)
+
+	check1 := common[1] - common[0]
+	if check1 == 0 {
+		return common[0]
+	}
+	check2 := common[1] / common[0]
+	flag := true
+	i := 1
+	for i < (total - 1) {
+		if (common[i+1] - common[i]) != check1 {
+			flag = false
+			break
+		}
+		i = i + 1
+	}
+
+	if flag {
+		return common[total-1] + check1
+	} else {
+		if (total%2 == 0) && check2 < 0 {
+			return (-1) * common[total-1] * check2
+		} else {
+			return common[total-1] * check2
+		}
+	}
+}
+
+func main() {
+	first := []int{1, 2, 3, 4}
+	second := []int{2, 4, 8}
+
+	fmt.Println(solution(first))
+	fmt.Println(solution(second))
+	//2023.03.15 결과
+	//테스트2 : 런타임에러 발생
+	//점수 : 88.9 / 100
+	//예외사항 처리 필요함
+}
