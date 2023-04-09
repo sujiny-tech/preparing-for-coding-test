af23918b438226c541feb3083db675e6410bfb70
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2023-04-09 19:09:21 +0900

    Add files via upload

---
+++ programmers/level1/GetMiddleLetter.go
@@ -0,0 +1,23 @@
+// package main
+
+// import "fmt"
+
+// func solution(s string) string {
+
+// 	if len(s)%2 == 0 {
+// 		index := len(s) / 2
+// 		return string(s[index-1 : index+1])
+// 	} else {
+// 		index := len(s) / 2
+// 		return string(s[index])
+// 	}
+// }
+
+// func main() {
+// 	first := solution("abcde")
+// 	fmt.Println(first)
+
+// 	first = solution("qwer")
+// 	fmt.Println(first)
+// }
+// //가운데 글자 가져오기
